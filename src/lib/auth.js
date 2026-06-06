338a7b0a4261f255614a4db15ead2876c572c250
mahmudul <mahmudul1106@gmail.com>
2026-06-07 00:31:34 +0600

    Vercel Deployment

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,49 +1,10 @@
-// import { betterAuth } from "better-auth";
-// import { MongoClient } from "mongodb";
-// import { mongodbAdapter } from "better-auth/adapters/mongodb";
-// import { jwt } from "better-auth/plugins";
-
-
-// const client = new MongoClient(process.env.MONGODB_URI);
-// const db = client.db('studyNook');
-
-// export const auth = betterAuth({
-//   database: mongodbAdapter(db, {
-//     // Optional: if you don't provide a client, database transactions won't be enabled.
-//     client
-//   }),
-// emailAndPassword: { 
-//     enabled: true, 
-//   }, 
-
-//   socialProviders: {
-//         google: { 
-//             clientId: process.env.GOOGLE_CLIENT_ID, 
-//             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-//               accessType: "offline", 
-//         prompt: "select_account consent",  
-//         }, 
-//     },
-//     session : {
-//     cookieCache: {
-//       enabled: true,
-//       strategy: "jwt",
-//       //max 7days
-//       maxAge: 7 * 24 * 60 * 60
-//     }
-//   },
-//   plugins: [
-//     jwt()
-//   ]
-
-// });
 
 import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 import { jwt } from "better-auth/plugins";
 
-const client = new MongoClient(process.env.MONGODB_URI);
+const client = new MongoClient(process.env.MONGODB_URI) || "http://localhost:3000";
 const db = client.db('studyNook');
 
 export const auth = betterAuth({
